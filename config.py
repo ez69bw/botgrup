8422bc2e6d4db8f6cc68ab9dc1d3fcf090186c3b
ez69bw <81008242+ez69bw@users.noreply.github.com>
2021-04-28 03:33:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION NAME = getenv("SESSION NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 
